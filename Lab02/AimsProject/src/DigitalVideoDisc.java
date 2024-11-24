efa7b668dc16a96352ac5f2cda46dcf54c46c2a0
HuyLong2k4 <maihuylong102@gmail.com>
2024-11-24 23:01:43 +0700

    print-cart

--- Lab02/AimsProject/src/DigitalVideoDisc.java
+++ Lab02/AimsProject/src/DigitalVideoDisc.java
@@ -1,36 +1,44 @@
 public class DigitalVideoDisc {
+    private int id;
     private String title;
     private String category;
     private String director;
     private int length;
     private float cost;
 
+    private static int nbDigitalVideoDiscs = 0;
+
+
     public DigitalVideoDisc(String title) {
-        super();
         this.title = title;
+        this.id = ++nbDigitalVideoDiscs;
     }
     public  DigitalVideoDisc(String category, String title, float cost) {
-        super();
         this.category = category;
         this.title = title;
         this.cost = cost;
+        this.id = ++nbDigitalVideoDiscs;
     }
 
     public DigitalVideoDisc(String director, String category, String title, float cost) {
-        super();
         this.director = director;
         this.category = category;
         this.title = title;
         this.cost = cost;
+        this.id = ++nbDigitalVideoDiscs;
     }
 
     public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
-        super();
         this.title = title;
         this.category = category;
         this.director = director;
         this.length = length;
         this.cost = cost;
+        this.id = ++nbDigitalVideoDiscs;
+    }
+
+    public boolean isMatch(String title) {
+        return this.title.equalsIgnoreCase(title);
     }
 
     public String getTitle() {
@@ -71,5 +79,15 @@ public class DigitalVideoDisc {
 
     public void setCost(float cost) {
         this.cost = cost;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return "ID: " + id + ", Title: " + title + ", Category: " + category + ", Director: " + director +
+                ", Length: " + length + " Price: $" + cost;
     }
 }
